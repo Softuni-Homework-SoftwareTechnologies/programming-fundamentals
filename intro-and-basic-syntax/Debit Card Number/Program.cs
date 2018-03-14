bdcada6876aa17716a8ac52c48f05af3d856fa14
Yancho-BB <yancho.cs@gmail.com>
2018-03-14 20:11:57 +0200

    Adding folder for problems solutions + 1st problem solution

---
+++ intro-and-basic-syntax/Debit Card Number/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Debit_Card_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var num1 = int.Parse(Console.ReadLine());
+            var num2 = int.Parse(Console.ReadLine());
+            var num3 = int.Parse(Console.ReadLine());
+            var num4 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("{0:0000} {1:0000} {2:0000} {3:0000}", num1, num2, num3, num4);
+        }
+    }
+}
