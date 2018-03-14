3249547abe23883260ea88b0fec8af74c88de05f
Petar Boyadzhiev <kpatyh@gmail.com>
2018-03-14 21:20:01 +0200

    Changed variables for Problems 2 and 3

--- intro-and-basic-syntax/RectangleArea.cs
+++ intro-and-basic-syntax/RectangleArea.cs
@@ -5,12 +5,12 @@ class RectangleArea
     static void Main(string[] args)
     {
         // First we get the input for width and we parse it making it into a type double.
-        double width = double.Parse(Console.ReadLine());
+        double wid = double.Parse(Console.ReadLine());
         // We do the same thing for the height with again type double.
-        double height = double.Parse(Console.ReadLine());
+        double hei = double.Parse(Console.ReadLine());
         //Then we create a new double variable which will be the calculation between the width multiplied by the height.
-        double area = width * height;
+        double ar = wid * hei;
         //Finally we print the area variable with $"{area:F2}" so the outcome will be formatted to the 2nd character after the decimal point.
-        Console.WriteLine($"{area:F2}");
+        Console.WriteLine($"{ar:F2}");
     }
 }
