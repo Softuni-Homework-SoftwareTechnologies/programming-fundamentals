6e46989bcce797b6268fd0be270b7ff741a33f15
Theodore Vezenkov <theodorevezenkov@gmail.com>
2018-03-14 20:42:24 +0200

    documenting problem 3

--- intro-and-basic-syntax/Miles to Kilometers/Program.cs
+++ intro-and-basic-syntax/Miles to Kilometers/Program.cs
@@ -10,8 +10,12 @@ namespace milesToKm
     {
         static void Main(string[] args)
         {
+			//Reading a floating point number from the Console
             double miles = double.Parse(Console.ReadLine());
+			//To find the distance in kilometres the distance in miles is multiplied by 1.60934 => 1 mile == 1.60934
+			//To be even more precise we use the data type 'double'
             double km = miles * 1.60934;
+			//And finally the result is formated to the 2nd decimal place.
             Console.WriteLine("{0:f2}", km);
         }
     }
