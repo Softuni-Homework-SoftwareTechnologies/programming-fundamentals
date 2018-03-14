3ae962680db73eee344dd09d21520face81ca79b
Theodore Vezenkov <theodorevezenkov@gmail.com>
2018-03-14 20:25:28 +0200

    added solution for problem 5

---
+++ intro-and-basic-syntax/CharacterStats.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class CharacterStats
+{
+    static void Main(string[] args)
+    {
+        string name = Console.ReadLine();
+        int currentHealth = int.Parse(Console.ReadLine());
+        int maxHealth = int.Parse(Console.ReadLine());
+        int currentEnergy = int.Parse(Console.ReadLine());
+        int maxEnergy = int.Parse(Console.ReadLine());
+
+        string health = new string('|', currentHealth);
+        string energy = new string('|', currentEnergy);
+        string missingHealth = new string('.', maxHealth - currentHealth);
+        string missingEnergy = new string('.', maxEnergy - currentEnergy);
+
+        Console.WriteLine($"Name: {name}");
+        Console.WriteLine($"Health: |{health}{missingHealth}|");
+        Console.WriteLine($"Energy: |{energy}{missingEnergy}|");
+    }
+}
